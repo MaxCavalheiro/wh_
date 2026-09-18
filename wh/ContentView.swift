cea5b412cacf818483c3af5c948960400bcebe32
Max Cavalheiro <2357618+MaxCavalheiro@users.noreply.github.com>
2026-09-18 10:45:25 -0300

    Initial Commit

---
+++ wh/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wh
+//
+//  Created by Max Cavalheiro on 18/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wh/whApp.swift
@@ -0,0 +1,17 @@
+//
+//  whApp.swift
+//  wh
+//
+//  Created by Max Cavalheiro on 18/09/26.
+//
+
+import SwiftUI
+
+@main
+struct whApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
